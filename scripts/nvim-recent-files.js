0ab2a0db2827ab3b957ba2aa3351132d215b0b8b
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-12-17 18:07:46 +0100

    patch

--- scripts/nvim-recent-files.js
+++ scripts/nvim-recent-files.js
@@ -7,8 +7,9 @@ const alfredMatcher = (str) => str.replace (/[-()_.:]/g, " ");
 //──────────────────────────────────────────────────────────────────────────────
 
 const jsonArray = app.doShellScript("zsh scripts/nvim-recent-files.sh")
+	.replace(/.*?(?=\/)/, "") // remove terminal characters
 	.split("\r")
-	.slice(1) // remove first element (useless output)
+	.slice(1) // more things to remove due to terminal characters
 	.map(filepath => {
 
 		const fileName = filepath.split("/").pop();
