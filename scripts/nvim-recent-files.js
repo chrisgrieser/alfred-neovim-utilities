8743f5083bbae7561be633ab57994ad788f52aeb
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-12-19 02:12:52 +0100

    better matching and 25 max for oldfiles

--- scripts/nvim-recent-files.js
+++ scripts/nvim-recent-files.js
@@ -2,29 +2,36 @@
 ObjC.import("stdlib");
 const app = Application.currentApplication();
 app.includeStandardAdditions = true;
-const alfredMatcher = (str) => str.replace (/[-()_.:]/g, " ");
+
+function alfredMatcher(str) {
+	const clean = str.replace(/[-()_.:#/\\;,[\]]/g, " ");
+	const camelCaseSeperated = str.replace(/([A-Z])/g, " $1");
+	return [clean, camelCaseSeperated, str].join(" ");
+}
+
+const numOfOldFiles = 25;
 
 //──────────────────────────────────────────────────────────────────────────────
 
-const jsonArray = app.doShellScript("zsh scripts/nvim-recent-files.sh")
+const jsonArray = app
+	.doShellScript("zsh scripts/nvim-recent-files.sh")
 	.replace(/.*?(?=\/)/, "") // remove terminal characters
 	.split("\r")
-	.slice(1) // more things to remove due to terminal characters
+	.slice(0, numOfOldFiles) // more things to remove due to terminal characters
 	.map(filepath => {
-
 		const fileName = filepath.split("/").pop();
 		const twoParents = filepath.replace(/.*\/(.*\/.*)\/.*$/, "$1");
 
 		return {
-			"title": fileName,
-			"match": alfredMatcher (fileName),
-			"subtitle": "▸ " + twoParents,
-			"type": "file:skipcheck",
-			"icon": {
-				"type": "fileicon",
-				"path": filepath,
+			title: fileName,
+			match: alfredMatcher(fileName),
+			subtitle: "▸ " + twoParents,
+			type: "file:skipcheck",
+			icon: {
+				type: "fileicon",
+				path: filepath,
 			},
-			"arg": filepath,
+			arg: filepath,
 		};
 	});
 
