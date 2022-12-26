bc9b0e998ae3c71e7ec07f057cdda23078ccc98d
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-12-26 17:34:44 +0100

    refactor: oldfiles retrival

--- scripts/nvim-recent-files.js
+++ scripts/nvim-recent-files.js
@@ -9,15 +9,27 @@ function alfredMatcher(str) {
 	return [clean, camelCaseSeperated, str].join(" ");
 }
 
-const numOfOldFiles = 25;
+function readFile(path) {
+	const fm = $.NSFileManager.defaultManager;
+	const data = fm.contentsAtPath(path);
+	const str = $.NSString.alloc.initWithDataEncoding(data, $.NSUTF8StringEncoding);
+	return ObjC.unwrap(str);
+}
+
+const fileExists = filePath => Application("Finder").exists(Path(filePath));
 
 //──────────────────────────────────────────────────────────────────────────────
 
-const jsonArray = app
-	.doShellScript("zsh scripts/nvim-recent-files.sh")
-	.replace(/.*?(?=\/)/, "") // remove terminal characters
-	.split("\r")
-	.slice(1, numOfOldFiles) // more things to remove due to terminal characters
+// generate recent files list
+const oldfiles = app.doShellScript(`
+	temp=/tmp/oldfiles.txt
+	[[ -e "$temp" ]] && rm "$temp"
+	nvim -c "redir > $temp | echo v:oldfiles | redir end | q" &>/dev/null
+	tr "'" '"' < "$temp" # single quores invalid in JSON
+`);
+
+const jsonArray = JSON.parse(oldfiles)
+	.filter(file => fileExists(file))
 	.map(filepath => {
 		const fileName = filepath.split("/").pop();
 		const twoParents = filepath.replace(/.*\/(.*\/.*)\/.*$/, "$1");
