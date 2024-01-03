69353bdf932f4aecc0dafcce822a7a88d1025686
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-01-03 16:56:46 +0100

    improv(pluginSearch): switch source from neovimcraft to awesome-neovim
    `neovimcraft` does not seem well maintained, and does not include recent
    plugins anymore. `awesome-neovim` is up-to-date and better maintained.
    Also, reading it's GitHub file seems to be quicker than accessing the
    `neovimcraft` API.
    
    However, `awesome-neovim` has the drawback that it does not include
    repo-statistics like stars or last repo update. As completeness has
    higher priority than detailedness, this workflow switches to
    awesome-neovim for now.

--- scripts/search-nvim-plugins.js
+++ scripts/search-nvim-plugins.js
@@ -8,8 +8,8 @@ app.includeStandardAdditions = true;
 /** @param {string} str */
 function alfredMatcher(str) {
 	const clean = str.replace(/[-_./]/g, " ");
-	const camelCaseSeperated = str.replace(/([A-Z])/g, " $1");
-	return [clean, camelCaseSeperated, str].join(" ") + " ";
+	const camelCaseSeparated = str.replace(/([A-Z])/g, " $1");
+	return [clean, camelCaseSeparated, str].join(" ") + " ";
 }
 
 /** @param {string} url */
@@ -20,74 +20,16 @@ function httpRequest(url) {
 	return requestString;
 }
 
-function ensureCacheFolderExists() {
-	const finder = Application("Finder");
-	const cacheDir = $.getenv("alfred_workflow_cache");
-	if (!finder.exists(Path(cacheDir))) {
-		console.log("Cache Dir does not exist and is created.");
-		const cacheDirBasename = $.getenv("alfred_workflow_bundleid");
-		const cacheDirParent = cacheDir.slice(0, -cacheDirBasename.length);
-		finder.make({
-			new: "folder",
-			at: Path(cacheDirParent),
-			withProperties: { name: cacheDirBasename },
-		});
-	}
-}
-
-/** @param {string} path */
-function cacheIsOutdated(path) {
-	const cacheAgeThresholdHours = 24; // 24h = cache is old
-	ensureCacheFolderExists();
-	const cacheObj = Application("System Events").aliases[path];
-	if (!cacheObj.exists()) return true;
-	const cacheAgeHours = (+new Date() - cacheObj.creationDate()) / 1000 / 60 / 60;
-	return cacheAgeHours > cacheAgeThresholdHours;
-}
-
-/**
- * @param {string} firstPath
- * @param {string} secondPath
- * @returns {boolean} firstPathOlderThanSecond
- */
-function olderThan(firstPath, secondPath) {
-	const firstMdate = +Application("System Events").aliases[firstPath].modificationDate();
-	const secondMdate = +Application("System Events").aliases[secondPath].modificationDate();
-	const firstPathOlderThanSecond = firstMdate - secondMdate < 0;
-	return firstPathOlderThanSecond;
-}
-
-/** @param {string} path */
-function readFile(path) {
-	const data = $.NSFileManager.defaultManager.contentsAtPath(path);
-	const str = $.NSString.alloc.initWithDataEncoding(data, $.NSUTF8StringEncoding);
-	return ObjC.unwrap(str);
-}
-
-/** @param {string} filepath @param {string} text */
-function writeToFile(filepath, text) {
-	const str = $.NSString.alloc.initWithUTF8String(text);
-	str.writeToFileAtomicallyEncodingError(filepath, true, $.NSUTF8StringEncoding, null);
-}
-
 //──────────────────────────────────────────────────────────────────────────────
 
-// INFO Not searching awesome neovim, since neovimscraft already scraps them
-// TODO search dotfyles, when their collection is more thorough
+// INFO Searching awesome-neovim instead of neovimcraft or dotfyle, since the
+// the latter two only scrape awesome-neovim anyway
 
 /** @type {AlfredRun} */
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
-	// UPDATE CACHE IF OUTDATED OR IF PLUGINS WERE INSTALLED/UNINSTALLED
+	// determine local plugins
 	const pluginInstallPath = $.getenv("plugin_installation_path");
-	const cachePath = $.getenv("alfred_workflow_cache") + "/neovimcraftPlugins.json";
-	if (!cacheIsOutdated(cachePath) && olderThan(pluginInstallPath, cachePath))
-		return readFile(cachePath);
-
-	//───────────────────────────────────────────────────────────────────────────
-	// REQUEST NEOVIMCRAFT
-
-	const neovimcraftURL = "https://nvim.sh/s";
 	const installedPlugins = app
 		.doShellScript(
 			`cd "${pluginInstallPath}" && grep --only-matching --no-filename --max-count=1 "http.*" ./*/.git/config`,
@@ -98,39 +40,29 @@ function run() {
 			return ownerAndName;
 		});
 
-	const pluginsArr = httpRequest(neovimcraftURL)
+	// awesome-neovim list
+	const awesomeNeovimList = "https://raw.githubusercontent.com/rockerBOO/awesome-neovim/main/README.md";
+
+	/** @type {AlfredItem[]} */
+	const pluginsArr = httpRequest(awesomeNeovimList)
 		.split("\n")
-		.slice(2)
 		.map((/** @type {string} */ line) => {
-			const parts = line.split(/ {2,}/);
-			const repo = parts[0];
-			const name = repo.split("/")[1];
+			if (!line.startsWith("- [") || !line.includes("/")) return {};
 
-			// subtitles
-			const stars = parts[1];
-			const openIssues = parts[2];
-			const daysAgo = Math.ceil((+new Date() - +new Date(parts[3])) / 1000 / 3600 / 24);
-			let updated =
-				daysAgo < 31
-					? daysAgo.toString() + " days ago"
-					: Math.ceil(daysAgo / 30).toString() + " months ago";
-			if (updated.startsWith("1 ")) updated = updated.replace("s ago", " ago"); // remove plural "s"
-			const desc = parts[4] || "";
-			let subtitle = `★ ${stars} – ${updated}`;
-			if (desc) subtitle += " – " + desc;
-
-			// install icon
+			const mdLinkRegex = /\[(.+?)\]\((.+?)\) - (.*)/;
+			const [_, repo, url, desc] = line.match(mdLinkRegex);
+			const [author, name] = repo.split("/");
 			const installedIcon = installedPlugins.includes(repo) ? " ✅" : "";
 
 			return {
 				title: name + installedIcon,
 				match: alfredMatcher(repo),
-				subtitle: subtitle,
-				arg: "https://github.com/" + repo,
+				subtitle: author + "  ·  " + desc,
+				arg: url,
 				uid: repo,
 				mods: {
 					shift: {
-						subtitle: `⇧: Search Issues (${openIssues} open)`,
+						subtitle: "⇧: Search Issues",
 						arg: "",
 						variables: { repoID: repo },
 					},
@@ -138,7 +70,5 @@ function run() {
 			};
 		});
 
-	const alfredResponse = JSON.stringify({ items: pluginsArr });
-	writeToFile(cachePath, alfredResponse);
-	return alfredResponse;
+	return JSON.stringify({ items: pluginsArr });
 }
