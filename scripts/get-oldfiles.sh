505763900cf18a90deeffbc958dc6beeab1795fb
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-02-09 22:47:32 +0100

    refactor: overhaul oldfiles

--- scripts/get-oldfiles.sh
+++ scripts/get-oldfiles.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env zsh
 
-# WARN do not embed this file into the js file, otherwise it does revtrieve the
+# WARN do not embed this file into the js file, otherwise it does retrieve the
 # proper oldfiles, since it then lacks information on the location of the shada
 # file for some reason
 temp=/tmp/oldfiles.txt
 [[ -e "$temp" ]] && rm "$temp"
 nvim -c "redir > $temp | echo v:oldfiles | redir end | q" &>/dev/null
-tr "'" '"' <"$temp" # single quotes invalid in json
+cat "$temp"
