061443b4ab4e9665988c899f4346fc2b9d51be33
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2025-08-03 08:19:40 +0200

    perf: better caching for plugin search

--- scripts/search-nvim-plugins.js
+++ scripts/search-nvim-plugins.js
@@ -20,6 +20,42 @@ function httpRequest(url) {
 
 const fileExists = (/** @type {string} */ filePath) => Application("Finder").exists(Path(filePath));
 
+function ensureCacheFolderExists() {
+	const finder = Application("Finder");
+	const cacheDir = $.getenv("alfred_workflow_cache");
+	if (finder.exists(Path(cacheDir))) return;
+	console.log("Cache directory does not exist and is created.");
+	const cacheDirBasename = $.getenv("alfred_workflow_bundleid");
+	const cacheDirParent = cacheDir.slice(0, -cacheDirBasename.length);
+	finder.make({
+		new: "folder",
+		at: Path(cacheDirParent),
+		withProperties: { name: cacheDirBasename },
+	});
+}
+
+/** @param {string} path */
+function cacheIsOutdated(path) {
+	const cacheAgeThresholdHours = 12; // CONFIG
+	const cacheObj = Application("System Events").aliases[path];
+	if (!cacheObj.exists()) return true;
+	const cacheAgeHours = (Date.now() - cacheObj.creationDate().getTime()) / 1000 / 60 / 60;
+	return cacheAgeHours > cacheAgeThresholdHours;
+}
+
+/** @param {string} filepath @param {string} text */
+function writeToFile(filepath, text) {
+	const str = $.NSString.alloc.initWithUTF8String(text);
+	str.writeToFileAtomicallyEncodingError(filepath, true, $.NSUTF8StringEncoding, null);
+}
+
+/** @param {string} path */
+function readFile(path) {
+	const data = $.NSFileManager.defaultManager.contentsAtPath(path);
+	const str = $.NSString.alloc.initWithDataEncoding(data, $.NSUTF8StringEncoding);
+	return ObjC.unwrap(str);
+}
+
 //──────────────────────────────────────────────────────────────────────────────
 
 // INFO Using the crawler result of `store.nvim`, since it is it includes more
@@ -42,53 +78,72 @@ const storeNvimList =
  * @property {{initial: string, lazyConfig: string}} install
  */
 
+/** @typedef {Object} StoreNvimData
+ * @property {StoreNvimRepo[]} items
+ * @property {{total_count: number, crawled_at: string}} meta
+ */
+//───────────────────────────────────────────────────────────────────────────
+
+/**
+ * @param {(AlfredItem & {fullRepo: string})[]} alfredItems
+ * @return {AlfredItem[]} updated items
+ */
+function addIconsForInstalledPlugins(alfredItems) {
+	const pluginInstallPath = $.getenv("plugin_installation_path");
+	if (!fileExists(pluginInstallPath)) return alfredItems;
+
+	let /** @type {string[]} */ installedPlugins = [];
+	const shellCmd = `cd "${pluginInstallPath}" && grep --only-matching --no-filename --max-count=1 "http.*" ./*/.git/config`;
+	installedPlugins = app
+		.doShellScript(shellCmd)
+		.split("\r")
+		.map((remote) => {
+			const ownerAndName = remote.split("/").slice(3, 5).join("/").slice(0, -4);
+			return ownerAndName;
+		});
+
+	const updatedItems = alfredItems.map((item) => {
+		if (installedPlugins.includes(item.fullRepo)) item.title += " ✅";
+		return item;
+	});
+	return updatedItems;
+}
+
 //──────────────────────────────────────────────────────────────────────────────
 
 /** @type {AlfredRun} */
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
-	// determine local plugins
-	const pluginInstallPath = $.getenv("plugin_installation_path");
-	let /** @type {string[]} */ installedPlugins = [];
-	if (fileExists(pluginInstallPath)) {
-		const shellCmd = `cd "${pluginInstallPath}" && grep --only-matching --no-filename --max-count=1 "http.*" ./*/.git/config`;
-		installedPlugins = app
-			.doShellScript(shellCmd)
-			.split("\r")
-			.map((remote) => {
-				const ownerAndName = remote.split("/").slice(3, 5).join("/").slice(0, -4);
-				return ownerAndName;
-			});
+	ensureCacheFolderExists();
+	const cachePath = $.getenv("alfred_workflow_cache") + "/nvimPluginsAlfredItems.json";
+
+	// use cached items (PERF not using Alfred's caching, so install icons can be updated)
+	if (!cacheIsOutdated(cachePath)) {
+		const alfredItems = JSON.parse(readFile(cachePath));
+		const itemsWithInstallIcons = addIconsForInstalledPlugins(alfredItems);
+		return JSON.stringify({ items: itemsWithInstallIcons });
 	}
 
-	// get data & log it
-	let nvimStoreData = [];
+	// fetch data
+	let /** @type {StoreNvimData} */ nvimStoreData;
 	try {
 		nvimStoreData = JSON.parse(httpRequest(storeNvimList));
 	} catch (_error) {
-		const item = { title: "Cannot retrieve data", valid: false };
-		return JSON.stringify({ items: [item] });
+		const errItem = { title: "Cannot retrieve data", valid: false };
+		return JSON.stringify({ items: [errItem] });
 	}
+	console.log("Fetched new data from store.nvim.");
 	console.log("Last crawl:", new Date(nvimStoreData.meta.crawled_at).toLocaleString());
 	console.log("Total plugins:", nvimStoreData.meta.total_count);
 
 	// construct Alfred items
-	const pluginsArr = JSON.parse(httpRequest(storeNvimList))
-		.items.sort(
-			(/** @type {StoreNvimRepo} */ a, /** @type {StoreNvimRepo} */ b) =>
-				new Date(b.pushed_at).getTime() - new Date(a.pushed_at).getTime(),
-		)
-		.map((/** @type {StoreNvimRepo} */ repo) => {
-			const {
-				full_name,
-				description,
-				html_url,
-				pretty_stargazers_count,
-				pretty_pushed_at,
-				install,
-			} = repo;
+	const alfredItems = nvimStoreData.items
+		.sort((a, b) => new Date(b.pushed_at).getTime() - new Date(a.pushed_at).getTime())
+		.map((repo) => {
+			// biome-ignore format: does not need to be read so often
+			const { full_name, description, html_url, pretty_stargazers_count, pretty_pushed_at, install } = repo;
 			const [author, name] = full_name.split("/");
-			const installedIcon = installedPlugins.includes(full_name) ? " ✅" : "";
+
 			const subtitle = [
 				"⭐ " + pretty_stargazers_count,
 				author,
@@ -97,13 +152,14 @@ function run() {
 			].join("  ·  ");
 			const lazyNvimInstall = install?.lazyConfig;
 
-			return {
-				title: name + installedIcon,
+			const /** @type {(AlfredItem & {fullRepo: string})} */ item = {
+				title: name,
+				fullRepo: full_name, // just for adding of the install icon adding
 				match: alfredMatcher(full_name),
 				subtitle: subtitle,
 				arg: html_url,
 				mods: {
-					cmd: { arg: repo }, // open help page
+					cmd: { arg: full_name }, // open help page
 					ctrl: {
 						arg: lazyNvimInstall,
 						valid: Boolean(lazyNvimInstall),
@@ -113,12 +169,11 @@ function run() {
 					},
 				},
 				quicklookurl: html_url,
-				uid: repo,
 			};
+			return item;
 		});
+	writeToFile(cachePath, JSON.stringify(alfredItems));
 
-	return JSON.stringify({
-		items: pluginsArr,
-		cache: { seconds: 300, loosereload: true }, // faster, to update install icons
-	});
+	const itemsWithInstallIcons = addIconsForInstalledPlugins(alfredItems);
+	return JSON.stringify({ items: itemsWithInstallIcons });
 }
